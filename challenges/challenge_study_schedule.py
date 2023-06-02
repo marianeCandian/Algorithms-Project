cee79efe6e779e615c5f673797f426345655e652
Mariane Candian <mariane.candian@gmail.com>
2023-06-02 16:19:09 -0300

    feat: implementando study_schedule para quantidade de estudades

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    estudantes_presentes = 0
+
+    for intervalo in permanence_period:
+        entrada, saida = intervalo
+
+        if type(entrada) != int or type(saida) != int:
+            return None
+
+        for tempo in range(entrada, saida + 1):
+            if tempo == target_time:
+                estudantes_presentes += 1
+
+    return estudantes_presentes
+
