97158dac4005eb30a0a3834db364a8c01feb7269
Mariane Candian <mariane.candian@gmail.com>
2023-06-13 11:02:22 -0300

    feat: criando a função find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if nums is None or not isinstance(nums, list):
+        return False
+
+    unique_nums = set()
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            return False
+        if num in unique_nums:
+            return num
+        unique_nums.add(num)
+    
+    return False
+
